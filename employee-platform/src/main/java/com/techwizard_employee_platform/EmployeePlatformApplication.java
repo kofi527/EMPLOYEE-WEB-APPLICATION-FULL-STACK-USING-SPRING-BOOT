049f0751eda57056ac5f1029f63646695256791a
kofi527 <63665356+kofi527@users.noreply.github.com>
2025-04-17 18:28:58 -0500

    Spring boot packages
    Updated packages with spring boot

---
+++ employee-platform/src/main/java/com/techwizard_employee_platform/EmployeePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.techwizard_employee_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePlatformApplication.class, args);
+	}
+
+}
