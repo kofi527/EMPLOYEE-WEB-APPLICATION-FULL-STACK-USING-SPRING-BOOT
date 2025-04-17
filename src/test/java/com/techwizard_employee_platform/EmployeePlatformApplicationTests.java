def055b0efacb42f4e1afee4143e59e908293426
kofi527 <63665356+kofi527@users.noreply.github.com>
2025-04-17 18:27:59 -0500

    Initial commit

---
+++ src/test/java/com/techwizard_employee_platform/EmployeePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techwizard_employee_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
