45ae9bdec75f11e42f2570cddd092abdca23f41d
kofi527 <63665356+kofi527@users.noreply.github.com>
2025-05-04 09:43:57 -0500

    Update EmployeePlatformApplication.java
     created packages and  configuration files for the backend

--- src/main/java/com/techwizard_employee_platform/EmployeePlatformApplication.java
+++ src/main/java/com/techwizard_employee_platform/EmployeePlatformApplication.java
@@ -1,4 +1,4 @@
-package com.techwizard_employee_platform;
+85-*package com.techwizard_employee_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
